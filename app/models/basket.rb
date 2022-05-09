251d1a5e1145a7991660b35ea5b89e65d80d5897
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-05-09 22:41:45 +0900

    Update: 全modelとmigrationファイルを編集

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,6 +1,6 @@
 class Basket < ApplicationRecord
   belongs_to :oyatsu
-  belongs_to :basket
+  belongs_to :user
 
   validates :quantity, numericality: { only_integer: true }
 end
--- db/migrate/20220509132953_create_baskets.rb
+++ db/migrate/20220509132953_create_baskets.rb
@@ -1,9 +1,9 @@
 class CreateBaskets < ActiveRecord::Migration[6.1]
   def change
     create_table :baskets do |t|
-      t.integer :oatsu_id
-      t.integer :user_id
       t.integer :quantitiy
+      t.references :oyatsu, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
