dd52eb075a5adb166f31a778acdd2b63deb267c4
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-05-13 18:06:21 +0900

    Add: db/seed.rb script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+include Scraping::Yaokin
+
+# おやつデータをDBに保存する
+item = Oyatsu.new.yaokin_oyatsu
+p item
+item.each do |i|
+  Oyatsu.find_or_create_by!(
+    name: i[:name]
+  ) do |oyatsu|
+    oyatsu.name = i[:name]
+    oyatsu.price = i[:price]
+    oyatsu.image = i[:image]
+  end
+end
+
