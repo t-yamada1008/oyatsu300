7b82a2a88b5e3d276f64846b325bd85746265a89
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-05-13 20:49:44 +0900

    Add: dwonload all image script and fix lib/scraping

--- lib/scraping/download_image.rb
+++ lib/scraping/download_image.rb
@@ -11,25 +11,33 @@ module Scraping
     end
 
     # ダウンロードメソッド
-    def download(image_url, base_url, location_path)
+    def download_image(image_url, genre)
       # いらないかもだけど念の為対象URLへの負荷対策としてsleepを仕込んでおく
       sleep 1
-      url = base_url + image_url
-      path = Rails.root.to_s + "/#{location_path}" + image_url
-      p url
-      p path
-      File.open(path, 'w+b') do |f|
-        URI.parse(url).open do |u|
+
+      # ファイルの保存先はapp/assets/images/download_imagesに固定
+      dir_download_images = "#{Rails.root}/app/assets/images/download_images"
+      Dir.mkdir dir_download_images unless Dir.exist?(dir_download_images)
+      # ジャンルごとにファイルの保存先を分ける
+      dir_path = "#{dir_download_images}/#{genre}"
+      Dir.mkdir dir_path unless Dir.exist?(dir_path)
+      # ファイル名を指定
+      file_name = "#{dir_path}/#{image_url.split('/').last}"
+
+      p image_url
+      p file_name
+      File.open(file_name, 'w+b') do |f|
+        URI.parse(image_url).open do |u|
           f.write(u.read)
         end
       end
     end
 
     # スクレイピングにて取得した画像URLを全てダウンロードする
-    def download_all(base_url, location_path)
+    def download_all_images
       oyatsu = Oyatsu.all
       oyatsu.each do |o|
-        download(o[:image], base_url, location_path)
+        download_image(o[:image], o[:genre])
       end
     end
   end
