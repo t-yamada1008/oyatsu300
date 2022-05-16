1a14c7cb9b8ac9f8b3e6ba5589fa7f6741b8840e
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-05-17 02:56:26 +0900

    Update: Oyatsu, Basekt

--- db/migrate/20220516110506_sorcery_core.rb
+++ db/migrate/20220516110506_sorcery_core.rb
@@ -2,8 +2,9 @@ class SorceryCore < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
+      t.integer :purse, null: false
       t.string :comment
-      # 今回はnullでもokにする。使わないので。
+      # 今回はnullでもokにする
       #t.string :email,            null: false, index: { unique: true }
       t.string :email
       t.string :crypted_password
