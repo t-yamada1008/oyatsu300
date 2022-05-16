b9024460cb6d9bb1e50304bb3b96054dfad660f2
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-05-16 19:06:33 +0900

    Add: User input and regist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
+# User
 class UsersController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to choose_oyatsu_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.failed')
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
