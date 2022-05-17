e1d0f096d04ecf864367e60b59e1d4f302dd6466
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-05-17 22:44:57 +0900

    Update: Basket calculation, create, destroy

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,4 +1,29 @@
 class BasketsController < ApplicationController
-  def index
+  before_action :set_basket, only: %i[destroy]
+
+  def create
+    @basket = Basket.new(basket_params)
+    @basket.user_id = current_user.id
+
+    if @basket.save
+      redirect_to choose_oyatsu_path, success: 'せいこう'
+    else
+      redirect_to choose_oyatsu_path, success: 'しっぱい'
+    end
+  end
+
+  def destroy
+    @basket.destroy!
+    redirect_to choose_oyatsu_path, success: 'せいこう'
+  end
+
+  private
+
+  def set_basket
+    @basket = Basket.find(params[:id])
+  end
+
+  def basket_params
+    params.require(:oyatsu).permit(:quantity, :oyatsu_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'users#new'
   get '/choose_oyatsu', to: 'oyatsus#index'
-  get 'baskets/index'
-  get 'oyatsus/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, only: %i[index show create]
+  resources :baskets, only: %i[new create update destroy]
 end
