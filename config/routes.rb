ea2ab11bbbab316b7e1132189685d0b45d6eed38
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-05-19 20:40:43 +0900

    Add: Everyone Oyatsu page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/choose_oyatsu', to: 'oyatsus#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: %i[index show create] do
-    resources :baskets, only: %i[index show new create update destroy], shallow: true
+  resources :users, only: %i[index new create edit update] do
+    resources :baskets, only: %i[index show new create destroy], shallow: true
   end
 end
