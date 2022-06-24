acc8beab9015cb086fdc4d5ebf487bd6ed2fee01
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-06-24 21:22:12 +0900

    Add: Adminログイン機能を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
-  def page_title(page_title = '')
-    base_title = t('.title')
+  def page_title(page_title = '', admin: false)
+    base_title = if admin
+                   'おやつ300管理画面'
+                 else
+                   '遠足のおやつは300円まで'
+                 end
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// default
+// --- start---
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../stylesheets/application';
-import '@fortawesome/fontawesome-free/js/all.js';
 
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+// ---end---
+require("@fortawesome/fontawesome-free")
 require("bootstrap");
-require("admin-lte");
