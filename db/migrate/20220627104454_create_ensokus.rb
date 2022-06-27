d0d52dcbc6095ed302a94e2542f5f5b16b25a2d8
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-06-27 21:16:16 +0900

    Revert "Revert "Add: Ensokuテーブルとモデルを追加""

---
+++ db/migrate/20220627104454_create_ensokus.rb
@@ -0,0 +1,11 @@
+class CreateEnsokus < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ensokus do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :parse, null: false
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
