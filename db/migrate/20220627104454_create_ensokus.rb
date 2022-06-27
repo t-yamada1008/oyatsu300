e81fc88f50330804994a2566a840b0df77b1713b
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-06-27 22:28:13 +0900

    Add: Ensokuテーブルにデフォルト値300を追加

--- db/migrate/20220627104454_create_ensokus.rb
+++ db/migrate/20220627104454_create_ensokus.rb
@@ -2,7 +2,7 @@ class CreateEnsokus < ActiveRecord::Migration[6.1]
   def change
     create_table :ensokus do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :purse, null: false
+      t.integer :purse, null: false, default: 300
       t.string :comment
 
       t.timestamps
