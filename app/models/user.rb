16178e36b2e3bb9685e7c916f0e585ef8d55a942
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-06-27 21:19:09 +0900

    Fix: Userモデルを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, uniqueness: true, allow_nil: true
-  validates :comment, length: { maximum: 65_535 }
 
   enum role: {
     general: 0,
