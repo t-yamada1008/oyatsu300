4a8ae740d551aa6f0761d86256d44c5dc1702f21
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-07-05 16:35:44 +0900

    Add: えらんだおやつ(詳細)画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,14 @@ Rails.application.routes.draw do
   delete  'logout',         to: 'user_sessions#destroy'
   get     '/choose_oyatsu', to: 'oyatsus#index'
 
-  resource :my_page, only: %i[show edit update]
-  resources :password_resets, only: %i[new create edit update]
-
-
   resource :users, only: %i[index new create] do
     resources :ensokus, shallow:true  do
       resources :baskets, only: %i[index show new create destroy], shallow: true
     end
   end
+
+  resource :my_page, only: %i[show edit update]
+  resources :password_resets, only: %i[new create edit update]
 
   namespace :admin do
     root              to: 'dashboards#index'
