3a4f7ad36f9f644cf7a818d7a4cd34ed8064974e
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-07-22 14:36:31 +0900

    Improve: Sessionを用いてユーザー未ログインでも遠足データのやりとりを可能にする

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,14 +2,15 @@ class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: %i[new create], raise: false
 
   def new
-    redirect_to new_users_ensoku_path if logged_in?
+    redirect_to ensokus_path if logged_in?
   end
 
   def create
     @user = login(params[:email], params[:password])
-
     if @user
-      redirect_back_or_to new_users_ensoku_path, success: t('.success')
+      binding.pry
+      set_ensoku_user if session[:ensoku_id].present?
+      redirect_back_or_to ensokus_path, success: t('.success')
     else
       flash.now[:danger] = t('.failure')
       render 'new'
@@ -19,5 +20,14 @@ class UserSessionsController < ApplicationController
   def destroy
     logout
     redirect_to root_path, success: t('.logout')
+  end
+
+  private
+
+  def set_ensoku_user
+    # 遠足のセッション情報が存在する場合、ユーザーと紐付けて登録
+    @ensoku = Ensoku.find(session[:ensoku_id])
+    @ensoku.user_id = @user.id
+    @ensoku.save
   end
 end
