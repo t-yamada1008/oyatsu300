a71fcaa1d5b24bd88ed46e43c95bd0f5fc5dafe6
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-07-25 14:12:03 +0900

    Update: 記述を更新。また、package.jsonは一つ前のコミット時に追加し忘れたので追加

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,12 +1,1 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import '../stylesheets/application';
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
-import("admin-lte/node_modules/bootstrap/dist/js/bootstrap.bundle.min");
-import("admin-lte/dist/js/adminlte.min");
+require('admin-lte')
