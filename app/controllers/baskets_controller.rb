2e4f23eb187a967b781452581120af4e9212ee8f
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-07-28 18:58:44 +0900

    Change: 残高オーバーの場合はモーダルウインドウを表示させるように変更

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -3,12 +3,10 @@ class BasketsController < ApplicationController
     @ensoku = Ensoku.find(params[:ensoku_id])
     @oyatsu = Oyatsu.find(params[:oyatsu_id])
     if @ensoku.purse_under_zero?
-      # TODO: Fix
-      redirect_to choose_oyatsu_path ensoku: @ensoku, success: 'てすとだよ'
+      render 'alert'
     else
       @ensoku.basket_in(@oyatsu)
       @ensoku.update_purse
-      # redirect_to choose_oyatsu_path ensoku: @ensoku, flash: { notice: 'ok' }
     end
   end
 
@@ -18,6 +16,5 @@ class BasketsController < ApplicationController
     @oyatsu = basket.oyatsu
     basket.destroy!
     @ensoku.update_purse
-    # redirect_to choose_oyatsu_path ensoku: @ensoku, flash: { notice: 'ok' }
   end
 end
