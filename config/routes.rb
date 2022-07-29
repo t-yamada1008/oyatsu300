332f931eca69dcdbf4184a238e0876f48eadec7c
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-07-29 20:14:43 +0900

    Add: adminで管理するroutesリソースを追加

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,8 @@ Rails.application.routes.draw do
     get     'login',  to: 'user_sessions#new'
     post    'login',  to: 'user_sessions#create'
     delete  'logout', to: 'user_sessions#destroy'
+    resources :users, shallow: true
+    resources :ensokus, shallow: true
+    resources :oyatsus, shallow: true
   end
 end
