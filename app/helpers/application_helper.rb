a97944fa209b4733386fc8454b77286e5c6f6155
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-08-09 16:34:35 +0900

    Fix: 画像保存先の情報変更に伴い、保存先をhelper経由で参照するように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # ページタイトル設定
   def page_title(page_title = '', admin: false)
     base_title = if admin
                    'おやつ300管理画面'
@@ -8,8 +9,19 @@ module ApplicationHelper
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 
+  # コントローラのパスを取得し、一致するする場合はactiveを返す
   def active_if(path)
-    # コントローラのパスを取得し、一致するする場合はactiveを返す
     path == controller_path ? 'active' : ''
+  end
+
+  # 画像ファイル呼び出し
+  def image_path(oyatsu)
+    # スクレイピングデータがあるときはそっちを優先
+    # スクレイピングデータがない場合はアップロード機能から呼び出し
+    if oyatsu.image_url.present?
+      "downloads/oyatsu/oyatsu_image/#{oyatsu.id}/#{oyatsu.image_url.split('/').last}"
+    else
+      oyatsu.oyatsu_image.url
+    end
   end
 end
