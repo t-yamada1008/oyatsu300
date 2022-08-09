4e7fefe9aa3363d75c61f4064ab2fb74a5163ae1
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-08-09 16:28:26 +0900

    Improve: スクレイピングライブラリをリファクタリング

--- lib/scraping/yaokin.rb
+++ lib/scraping/yaokin.rb
@@ -2,104 +2,108 @@ require 'open-uri'
 require 'nokogiri'
 
 # スクレイピングライブラリ
-module Scraping
-  # やおきん用
-  module Yaokin
-    include Scraping::ScrapingCore
-    include Scraping::DownloadImage
-    def check_scraping_yaokin
-      p '読み込めてるよ！'
-    end
+# 取得したデータをOyatsuテーブルに保存する
+# やおきん用
+module Scraping::Yaokin
+  # 定数
+  # 遷移元URL
+  BASE_URL = 'http://www.yaokin.com'.freeze
+  # 全ての商品URL
+  PRODUCTS_URL = 'http://www.yaokin.com/products.html'.freeze
 
+  include Scraping::ScrapingCore
+  include Scraping::DownloadImage
+
+  # 確認用スクリプト
+  def check_scraping_yaokin
+    p '読み込めてるよ！'
+  end
+
+  # やおきんのおかしデータを取得する
+  def yaokin_oyatsu
+    # 全ての商品htmlを取得してパース
+    products_doc = parse_document(PRODUCTS_URL)
+    products_a = products_doc.css('a')
     # ジャンルのURLを取得する
-    def push_genre_url(documents_a, base_url, target_url)
-      documents_a.each do |a|
-        if a.attribute('href').present?
-          href = a.attribute('href').value
-          target_url.push base_url + href if (href.include? '/products_search') && (href.exclude? 'http://') && (href.exclude? 'goods')
-        end
-      end
-    end
-
+    genre_url_arr = genre_url(products_a)
     # 商品のURLを取得する
-    def push_item_url(documents_a, base_url, target_url)
-      documents_a.each do |a|
+    item_url_arr = item_url(genre_url_arr)
+    # 商品のデータを取得する
+    item_info(item_url_arr)
+  end
+
+  private
+
+  # ジャンルのURLを取得する
+  def genre_url(products_a)
+    genre_url_arr = []
+    products_a.each do |a|
+      next unless a.attribute('href').present?
+
+      href = a.attribute('href').value
+      genre_url_arr.push BASE_URL + href if (href.include? '/products_search') && (href.exclude? 'http://') && (href.exclude? 'goods')
+    end
+    genre_url_arr
+  end
+
+  # 商品のURLを取得する
+  def item_url(genre_url_arr)
+    item_url_arr = []
+    # 商品ジャンルのリンクから単体商品のリンクを取得する
+    genre_url_arr.each do |g|
+      # ジャンルhtmlを取得してパース
+      genre_doc = parse_document(g)
+
+      # ジャンルhtmlのa要素から単体商品のリンクだけを取得する
+      genre_doc_a = genre_doc.css('a')
+      genre_doc_a.each do |a|
         if a.attribute('href').present?
           href = a.attribute('href').value
-          target_url.push base_url + href if href.include? '/item'
+          item_url_arr.push BASE_URL + href if href.include? '/item'
         end
       end
     end
+    item_url_arr
+  end
 
-    # やおきんのおかしデータを取得する
-    def yaokin_oyatsu
-      # 遷移元URL
-      base_url = 'http://www.yaokin.com'
-      # 全ての商品URL
-      products_url = 'http://www.yaokin.com/products.html'
-      # ジャンルurl
-      genre_url = []
-      # 商品url
-      item_url = []
-      # 商品
-      item = []
+  # 商品のデータを取得する
+  def item_info(item_url_arr)
+    item = []
+    item_url_arr.each do |i|
+      # 商品データ
+      item_data = {}
+      # アイテムhtmlを取得してパース
+      item_doc = parse_document(i)
 
-      # 全ての商品htmlを取得してパース
-      products_doc = parse_document(products_url)
+      # item_docの取得に失敗した場合は次のループへ
+      next if item_doc.nil?
 
-      # 全ての商品htmlのa要素から商品ジャンルのリンクだけを取得する
-      products_a = products_doc.css('a')
-      push_genre_url(products_a, base_url, genre_url)
+      # 商品名を取得
+      item_data[:name] = item_doc.css('.verlign_m')[1].children.attribute('alt').value
 
-      # 商品ジャンルのリンクから単体商品のリンクを取得する
-      genre_url.each do |g|
-        # ジャンルhtmlを取得してパース
-        genre_doc = parse_document(g)
+      # 商品価格を取得
+      item_price = item_doc.css('p')[6].children[1].text
 
-        # ジャンルhtmlのa要素から単体商品のリンクだけを取得する
-        genre_a = genre_doc.css('a')
-        push_item_url(genre_a, base_url, item_url)
-      end
+      # フリープライスの品物の場合、次のループへ
+      next if item_price.include?('フリープライス')
 
-      item_url.each do |i|
-        # 商品データ
-        item_data = {}
-        # アイテムhtmlを取得してパース
-        item_doc = parse_document(i)
+      # '円'を切り取りIntegerに変換
+      item_price = item_price.chop.to_i
 
-        # item_docの取得に失敗した場合は次のループへ
-        next if item_doc.nil?
+      # 300円以上の品物の場合、次のループへ
+      next if item_price > 300
 
-        # 商品名を取得
-        item_name = item_doc.css('.verlign_m')[1].children.attribute('alt').value
+      item_data[:price] = item_price
 
-        # 商品ジャンルを取得
-        item_genre = i.split('/').pop(2).first
+      # 商品イメージのURLを取得
+      item_data[:image_url] = BASE_URL + item_doc.css('.verlign_m')[1].children.attribute('src').value
 
-        # 商品価格を取得
-        item_price = item_doc.css('p')[6].children[1].text
+      # 商品イメージののファイル名を取得
+      item_data[:oyatsu_image] = item_data[:image_url].split('/').last
 
-        # フリープライスの品物の場合、次のループへ
-        next if item_price.include?('フリープライス')
-
-        # '円'を切り取りIntegerに変換
-        item_price = item_price.chop.to_i
-
-        # 300円以上の品物の場合、次のループへ
-        next if item_price > 300
-
-        # 商品イメージのURLを取得
-        item_image_url = base_url + item_doc.css('.verlign_m')[1].children.attribute('src').value
-
-        # 商品データを確定
-        item_data[:name] = item_name
-        item_data[:genre] = item_genre
-        item_data[:price] = item_price
-        item_data[:image_url] = item_image_url
-        item.push item_data
-        p item_data
-      end
-      item
+      p item_data
+      item.push item_data
     end
+    item
   end
 end
