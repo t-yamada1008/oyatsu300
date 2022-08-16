1d79e008c1cf013661e5ccd9aedf23845e73398d
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-08-16 20:38:06 +0900

    Add: time_zoneを追加

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,9 @@ module EnsokuNoOyatsuHa300YenMade
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+    config.eager_load_paths << Rails.root.join("extras")
 
     # rails g 時のファイル作成の可否を設定
     config.generators do |g|
