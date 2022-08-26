8b360bdfe1bc555472be6a1d6989d2e4e06f6678
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-08-26 20:40:47 +0900

    Fix: 金額計算をsessionを用いたものに修正

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,58 +1,53 @@
 class BasketsController < ApplicationController
   before_action :set_oyatsu, only: %i[basket_in basket_out]
-  def create
-    @ensoku = Ensoku.find(params[:ensoku_id])
-    @oyatsu = Oyatsu.find(params[:oyatsu_id])
-    if @ensoku.purse_under_zero?
-      render 'alert'
-    else
-      @ensoku.basket_in(@oyatsu)
-      @ensoku.update_purse
-    end
-  end
-
-  def destroy
-    basket = Basket.find(params[:id])
-    @ensoku = basket.ensoku
-    @oyatsu = basket.oyatsu
-    basket.destroy!
-    @ensoku.update_purse
-  end
 
   def basket_in
+    # 変数初期化
     results_arr = []
     oyatsus_arr = session[:oyatsus]
-    # セッションに何も存在しない場合
+    purse = session[:purse]
+    session[:basket_error_message].clear if session[:basket_error_message].present?
+    ## 金額チェック
+    # そもそも残りのおこづかいが0円
+    # result_purseでzero?判定するとちょうど残高0円になる場合も弾いてしまうため
+    return session[:basket_error_message] = t('.purse_over') if purse.zero?
+
+    # 計算
+    result_purse = purse - @oyatsu.price
+    # 0円以下だと弾く
+    return session[:basket_error_message] = t('.purse_over') if result_purse.negative?
+
+    ## おやつ追加チェック
+    # セッションにおやつが何も存在しない場合
     if oyatsus_arr.blank?
       results_arr.push({ oyatsu_id: @oyatsu.id, quantitiy: 1 })
+    # oyatsusに@oyatsu.idが含まれていない場合
+    elsif oyatsus_arr.find { |f| f[:oyatsu_id] == @oyatsu.id }.blank?
+      # 既存のおやつに新規のおやつをpush
+      oyatsus_arr.push({ oyatsu_id: @oyatsu.id, quantitiy: 1 })
+      results_arr = oyatsus_arr
+    # oyatsusに@oyatsu.idが含まれている場合
     else
-      # oyatsusに@oyatsu.idが含まれていない場合
-      if oyatsus_arr.find { |f| f['oyatsu_id'] == @oyatsu.id }.blank?
-        # 既存のおやつに新規のおやつをpush
-        oyatsus_arr.push({ oyatsu_id: @oyatsu.id, quantitiy: 1 })
-        results_arr = oyatsus_arr
-      # oyatsusに@oyatsu.idが含まれている場合
-      else
-        oyatsus_arr.each do |oyatsu|
-          oyatsu_id = oyatsu['oyatsu_id'].to_i
-          quantitiy = if oyatsu_id == @oyatsu.id
-                        oyatsu['quantitiy'].to_i + 1
-                      else
-                        oyatsu['quantitiy'].to_i
-                      end
-          results_arr.push({ oyatsu_id:, quantitiy: })
-        end
+      oyatsus_arr.each do |oyatsu|
+        oyatsu_id = oyatsu[:oyatsu_id].to_i
+        quantitiy = if oyatsu_id == @oyatsu.id
+                      oyatsu[:quantitiy].to_i + 1
+                    else
+                      oyatsu[:quantitiy].to_i
+                    end
+        results_arr.push({ oyatsu_id:, quantitiy: })
       end
     end
     session[:oyatsus] = results_arr
+    session[:purse] = result_purse
   end
 
   def basket_out
     results_arr = []
     oyatsus_arr = session[:oyatsus]
     oyatsus_arr.each do |oyatsu|
-      oyatsu_id = oyatsu['oyatsu_id'].to_i
-      quantitiy = oyatsu['quantitiy'].to_i
+      oyatsu_id = oyatsu[:oyatsu_id].to_i
+      quantitiy = oyatsu[:quantitiy].to_i
       # おやつidが一致する場合は計算処理
       quantitiy -= 1 if oyatsu_id == @oyatsu.id
       # 0になる場合はresultsにpushしない
@@ -62,6 +57,8 @@ class BasketsController < ApplicationController
       results_arr.push({ oyatsu_id:, quantitiy: })
     end
     session[:oyatsus] = results_arr
+    # バスケットにおやつの値段を追加
+    session[:purse] += @oyatsu.price
   end
 
   private
