0db5c651b0ed9c41962866d40c46145059c0fa96
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-08-29 19:45:07 +0900

    Change: ensoku_sessionsの変更に伴い、ensoku_controllerのCRUDにてデータ保存ができるよう変更

--- app/controllers/ensokus_controller.rb
+++ app/controllers/ensokus_controller.rb
@@ -1,6 +1,6 @@
 class EnsokusController < ApplicationController
+  before_action :set_oyatsus, only: %i[new]
   before_action :set_ensoku, only: %i[show edit update destroy]
-  before_action :check_request, only: %i[show edit update destroy]
 
   # 遠足一覧画面
   # Userの全遠足結果を取得
@@ -9,20 +9,24 @@ class EnsokusController < ApplicationController
   end
 
   # 選択結果の作成画面
-  def new; end
+  def new
+    @ensoku = Ensoku.new
+  end
 
   # セッション情報から遠足を新規作成
   def create
     # トランザクションをかけてensokuとbasketを一括保存
     ActiveRecord::Base.transaction do
-      # 例外が発生するかもしれない処理
       # ensokuを作成し保存
+      @ensoku = Ensoku.create(purse: session[:purse])
       # ensokuに紐づくoyatsuをbasketに保存
+      session_oyatsus = session[:oyatsus]
+      session_oyatsus.each do |s_oyatsu|
+        Basket.create(oyatsu_id: s_oyatsu[:oyatsu_id], ensoku_id: @ensoku.id, quantity: s_oyatsu[:quantity])
+      end
     end
-      # 例外が発生しなかった場合の処理
-    # redirect_to oyatsus_path, success: t('.success')
-    rescue => e
-      # 例外が発生した場合の処理
+    session[:ensoku] = @ensoku
+    redirect_to ensoku_path(@ensoku), success: t('.success')
   end
 
   # おかし選択結果
@@ -52,6 +56,16 @@ class EnsokusController < ApplicationController
 
   def set_ensoku
     @ensoku = Ensoku.find(params[:id])
+  end
+
+  # 選択されてるおやつだけをset。数量は気にしない。
+  def set_oyatsus
+    session_oyatsus = session[:oyatsus]
+    oyatsu_id_arr = []
+    session_oyatsus.each do |s_oyatsu|
+      oyatsu_id_arr.push(s_oyatsu[:oyatsu_id])
+    end
+    @oyatsus = Oyatsu.find(oyatsu_id_arr)
   end
 
   def ensoku_params
