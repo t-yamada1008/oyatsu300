347c2fc6ae8c7b848be878c758bc41aec27773ac
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-09-10 14:34:38 +0900

    Add: パスワードに最大文字数のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 128 }
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true, length: { minimum: 4 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, presence: true, length: { minimum: 4, maximum: 16 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, uniqueness: true, allow_nil: true
