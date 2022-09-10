171f8d4964135ee1238816dc0e742b981d55923d
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-09-10 17:30:24 +0900

    Fix: Basketモデルにnull制約がついていなかったので追加修正

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,6 +1,10 @@
 class Basket < ApplicationRecord
-  include MigrateMvp::MigrateBasket
-
   belongs_to :oyatsu
   belongs_to :ensoku
+
+  with_options presence: true do
+    validates :ensoku_id
+    validates :oyatsu_id
+    validates :quantity
+  end
 end
