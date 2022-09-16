6f5321dd6eb4f11bba6e0b4b8700d7a5f439e1c8
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-09-16 21:14:56 +0900

    Add: 各金額におけるtraitを追加

--- test/factories/oyatsus.rb
+++ test/factories/oyatsus.rb
@@ -2,5 +2,29 @@ FactoryBot.define do
   factory :oyatsu do
     sequence(:name, 'oyatsu_1')
     price { '10' }
+
+    trait :y50 do
+      price { '50' }
+    end
+
+    trait :y100 do
+      price { '100' }
+    end
+
+    trait :y150 do
+      price { '150' }
+    end
+
+    trait :y200 do
+      price { '200' }
+    end
+
+    trait :y250 do
+      price { '250' }
+    end
+
+    trait :y300 do
+      price { '300' }
+    end
   end
 end
