2e607ca200142f39114ef8ffd3c516053c2d17f6
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-09-23 15:22:22 +0900

    Add: user_sessionのspecを追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,9 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  before do
-    driven_by(:rack_test)
+
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+    xcontext 'Twitterでのログイン' do
+      it 'ログイン処理が成功する' do
+        # rootに遷移
+        visit root_path
+        # トグルを押すとリンクが表示
+        find('.navbar-toggler-icon').click
+        click_link 'ろぐいん'
+        # twitter
+        click_link 'Twitter'
+        expect(page).to have_content 'ろぐいんしたよ'
+        expect(current_path).to eq root_path
+      end
+
+      it 'ログイン処理が失敗する' do
+
+      end
+    end
+
+    context 'フォームでのログイン' do
+      it 'ログイン処理が成功する' do
+        # rootに遷移
+        visit root_path
+        # トグルを押すとリンクが表示
+        find('.navbar-toggler-icon').click
+        click_link 'ろぐいん'
+        fill_in 'めーるあどれす', with: user.email
+        fill_in 'ぱすわーど', with: 'password'
+        click_button 'ろぐいん'
+        expect(page).to have_content 'ろぐいんしたよ'
+        expect(current_path).to eq root_path
+      end
+
+      it 'ログイン処理が失敗する' do
+        # rootに遷移
+        visit root_path
+        # トグルを押すとリンクが表示
+        find('.navbar-toggler-icon').click
+        click_link 'ろぐいん'
+        fill_in 'めーるあどれす', with: ''
+        fill_in 'ぱすわーど', with: ''
+        click_button 'ろぐいん'
+        expect(page).to have_content 'ろぐいんしっぱい！もういちどためしてみてね'
+        expect(current_path).to eq login_path
+      end
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン後' do
+    context 'ログアウト' do
+      it 'ログアウトが成功する' do
+        login_as(user)
+        find('.navbar-toggler-icon').click
+        click_link 'ろぐあうと'
+        expect(page).to have_content 'ろぐあうとしたよ'
+        expect(current_path).to eq root_path
+      end
+    end
+  end
 end
