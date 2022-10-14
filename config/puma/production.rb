ed8c24a2ea34e37e6599234ae2845a7202b8c579
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2022-10-14 19:28:05 +0900

    Fix: deployの設定を編集

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -8,6 +8,6 @@ workers 2
 preload_app!
 
 pidfile "#{tmp_path}/pids/puma.pid"
-stdout_redirect "#{tmp_path}/logs/puma.stdout.log", "#{tmp_path}/logs/puma.stderr.log", true
+#stdout_redirect "#{tmp_path}/logs/puma.stdout.log", "#{tmp_path}/logs/puma.stderr.log", true
 
 plugin :tmp_restart
