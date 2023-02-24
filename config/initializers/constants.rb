68347b03f84c224905afdaa8f708fafd2cff8d4f
takatoshiyamada <takatoshiyamada.3505@gmail.com>
2023-02-24 11:57:50 +0900

    Delete: constantを再度削除

--- config/initializers/constants.rb
+++
@@ -1,8 +0,0 @@
-module Constants
-  ## Constants::NUMでアクセスできる
-  DEFAULT_URL_OPTIONS = if Rails.env == "production"
-                          'https://oyatsu300.com'
-                        else
-                          'http://127.0.0.1:3000/'
-                        end
-end
